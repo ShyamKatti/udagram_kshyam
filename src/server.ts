678ae9313a7c2c7389dee6359ccb1efd917046c1
Shyam <shyamstartup@gmail.com>
2021-01-04 21:04:19 +0530

    Commit for review

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import express, {Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get("/filteredimage", async(req: Request, res: Response) => {
+    const {image_url} = req.query;
+
+    if (!image_url) {
+      return res.status(400).send("image_url is required param");
+    }
+    const filteredReturnVal = await filterImageFromURL(image_url);
+    if (filteredReturnVal) {
+      return res.status(200).sendFile(filteredReturnVal);
+    } else {
+      return res.status(500).send(
+         {
+           "error": {
+             "message": "Something went wrong while trying to filter the image"
+           }
+        }
+      )
+    }
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
